6ad044562470664c427c08168c4b57a341d732c9
Dmitry@HP-Spectre-360-Dim <IT-Dim@web.de>
2022-10-26 11:51:34 +0200

    InputStreamReader

---
+++ Example_Homework_byte_26.10.2022/src/task5/Main.java
@@ -0,0 +1,20 @@
+package task5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Input string");
+//        String str = reader.readLine();
+
+//        while (true) {
+//            int x = System.in.read(); // System.in - считывает байты!
+//            System.out.println(x);
+//        }
+
+    }
+}
