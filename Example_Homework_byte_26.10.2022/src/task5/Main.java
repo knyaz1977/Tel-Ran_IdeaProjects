a8c510965476bf6f20746d9350974d81840456aa
Dmitry@HP-Spectre-360-Dim <IT-Dim@web.de>
2022-10-26 12:00:19 +0200

    new BufferedReader(new InputStreamReader(System.in))

--- Example_Homework_byte_26.10.2022/src/task5/Main.java
+++ Example_Homework_byte_26.10.2022/src/task5/Main.java
@@ -1,20 +1,28 @@
 package task5;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); // InputStreamReader - переводит байты в значения(char)
 //        System.out.println("Input string");
 //        String str = reader.readLine();
 
+        // Для тестирования можно раскомментировать!
 //        while (true) {
 //            int x = System.in.read(); // System.in - считывает байты!
 //            System.out.println(x);
 //        }
 
+        InputStreamReader isr = new InputStreamReader(System.in);
+        int x = isr.read();
+        System.out.println((char) x);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
     }
 }
